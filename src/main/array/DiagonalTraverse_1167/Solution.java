222e2344effe60a7882f4b511a325441cd8b3bd6
Vlad Petrov <retqqqq@gmail.com>
2021-05-25 01:16:16 +0200

    1167 solution

---
+++ src/main/array/DiagonalTraverse_1167/Solution.java
@@ -0,0 +1,43 @@
+package array.DiagonalTraverse_1167;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Solution {
+    public int[] findDiagonalOrder(int[][] mat) {
+        if (mat == null || mat.length == 0) {
+            return new int[0];
+        }
+
+        int m = mat.length;
+        int n = mat[0].length;
+
+        int[] result = new int[m * n];
+        List<Integer> temp = new ArrayList<>();
+
+        int resultIndex = 0;
+
+        for (int i = 0; i < (m + n) - 1; i++) {
+
+            temp.clear();
+
+            int row = i < n ? 0 : (i - n) + 1;
+            int col = i < n ? i : n - 1;
+
+            while (row < m && col > -1) {
+                temp.add(mat[row++][col--]);
+            }
+
+            if (i % 2 == 0) {
+                Collections.reverse(temp);
+            }
+
+            for (Integer integer : temp) {
+                result[resultIndex++] = integer;
+            }
+        }
+
+        return result;
+    }
+}
---
+++ src/test/array/DiagonalTraverse_1167/Tests.java
@@ -0,0 +1,36 @@
+package array.DiagonalTraverse_1167;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class Tests {
+    private Solution solution;
+
+    @BeforeEach
+    public void setUp() {
+        solution = new Solution();
+    }
+
+    @ParameterizedTest
+    @MethodSource("provideTestData")
+    public void testPositive(int[][] input, int[] expected) {
+        int[] actual = solution.findDiagonalOrder(input);
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    private static Stream<Arguments> provideTestData() {
+        return Stream.of(
+                Arguments.of(new int[][]{{1,2,3}, {4,5,6}, {7,8,9}}, new int[]{1,2,4,7,5,3,6,8,9}),
+                Arguments.of(new int[][]{{1,2}, {3,4}, {5,6}, {7,8}}, new int[]{1,2,3,5,4,6,7,8}),
+                Arguments.of(new int[][]{{1}, {2}, {3}, {4}, {5}}, new int[]{1,2,3,4,5}),
+                Arguments.of(new int[][]{}, new int[0]),
+                Arguments.of(null, new int[0])
+        );
+    }
+}
